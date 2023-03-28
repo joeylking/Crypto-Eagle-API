f8ec94ca5084091d3f1dfc24874e1a8c779ee98c
jsmith3763 <jsmith3763@gmail.com>
2023-03-28 08:51:26 -0700

    Writing CRUD routes

--- src/main/java/com/galvanize/team_1/User/UserController.java
+++ src/main/java/com/galvanize/team_1/User/UserController.java
@@ -1,4 +1,30 @@
 package com.galvanize.team_1.User;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class UserController {
+
+    UserService userService;
+
+    @PostMapping("/api/createuser")
+    public User createUser(@RequestBody User user) { return userService.addUser(); }
+
+    @DeleteMapping("/api/deleteuser/{id}")
+    public ResponseEntity deleteUser(@PathVariable int id) {
+        try {
+            userService.deleteUser(id);
+        }catch (UserNotFoundException e) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.accepted().build();
+    }
+
+    @PutMapping("/api/updatepassword/{id}")
+    public User updatePassword(@PathVariable int id,
+                               @RequestBody UpdatePasswordRequest update) {
+
+    }
 }
