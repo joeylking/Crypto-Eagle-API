e5acdff7e4686a3192d1426d76808021139c3f42
neoTemores <neo.temores@live.com>
2023-03-29 09:58:36 -0700

    debug compile error

--- src/main/java/com/galvanize/team_1/comments/CommentRepository.java
+++ src/main/java/com/galvanize/team_1/comments/CommentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
-    CommentList findAllByPostId(int parseInt);
+    CommentList findAllByPostId(int id);
 }
